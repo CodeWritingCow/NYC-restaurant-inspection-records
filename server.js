3fe8af1c1c3b432e1d42c0a46170f346ffd81da7
Gary Pang <gary.pang@gmail.com>
2017-12-13 02:39:39 -0500

    Rewrite server.js in ES6
    Issue #1

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express'),
-	app = express(),
-	morgan = require('morgan'), // morgan is a HTTP request logger middleware
-	request = require('request'), // request makes HTTP calls
-	bodyParser = require('body-parser'); // adds body object to request so app can access POST parameters
+const express 		= require('express');
+const app 			= express();
+const morgan 		= require('morgan'); // morgan is a HTTP request logger middleware
+const request 		= require('request'); // request makes HTTP calls
+const bodyParser 	= require('body-parser'); // adds body object to request so app can access POST parameters
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,15 +12,15 @@ app.use(morgan('dev'));
 
 app.use(express.static(__dirname));
 
-var port = process.env.PORT || 8080;
-var token = process.env.TOKEN;
+const port = process.env.PORT || 8080;
+const token = process.env.TOKEN;
 
 // SET THE ROUTES
 // =======================================
 // "req, res" stand for "request, response."
 
-app.get('/', function(req, res) {
-	request('https://data.cityofnewyork.us/resource/43nn-pn8j.json?boro=queens', function(error, response, body){
+app.get('/', (req, res) => {
+	request('https://data.cityofnewyork.us/resource/43nn-pn8j.json?boro=queens', (error, response, body) =>{
 		if (!error && response.statusCode === 200) {
 			res.send(body);
 		} else {
@@ -29,8 +29,8 @@ app.get('/', function(req, res) {
 	});
 });
 
-app.get('/chinese', function(req, res) {
-	request('https://data.cityofnewyork.us/resource/43nn-pn8j.json?cuisine_description=chinese', function(error, response, body){
+app.get('/chinese', (req, res) => {
+	request('https://data.cityofnewyork.us/resource/43nn-pn8j.json?cuisine_description=chinese', (error, response, body) =>{
 		if (!error && response.statusCode === 200) {
 			res.send(body);
 		} else {
@@ -39,8 +39,8 @@ app.get('/chinese', function(req, res) {
 	});
 });
 
-app.get('/flushing', function(req, res) {
-	request('https://data.cityofnewyork.us/resource/43nn-pn8j.json?zipcode=11358', function(error, response, body){
+app.get('/flushing', (req, res) => {
+	request('https://data.cityofnewyork.us/resource/43nn-pn8j.json?zipcode=11358', (error, response, body) => {
 		if (!error && response.statusCode === 200) {
 			res.send(body);
 		} else {
@@ -51,6 +51,6 @@ app.get('/flushing', function(req, res) {
 
 
 // Start server
-app.listen(port, function(){
-	console.log('App is running on http://localhost:' + port);
+app.listen(port, () => {
+	console.log(`App is running on http://localhost: ${port}`);
 });

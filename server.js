2db6d34b0aa335ab0f6fb1961830a964dba96ed0
Gary Pang <gary.pang@gmail.com>
2017-12-28 03:02:45 -0500

    Add Socrata query function to POST /search
    Issue #15

--- server.js
+++ server.js
@@ -85,7 +85,14 @@ app.post('/search', (req, res) => {
 	var borough = req.body.borough;
 
 	var data = req.body;
-	var businessName = data.dba.toLowerCase();
+	var businessName = data.dba.toUpperCase();
+
+	var socrataQuery = `$$app_token=${token}`;
+	// if data.dba contains a value, add socrataQuery to urlQuery
+
+	if (data.dba.length > 0) {
+		socrataQuery += `&$where=DBA%20like%20%27%25${businessName}%25%27`;
+	}
 
 	// Remove business name query
 	delete data.dba;
@@ -99,7 +106,7 @@ app.post('/search', (req, res) => {
 	var urlQuery = querystring.stringify(_.merge(data));
 
 	console.log(urlQuery);
-	console.log(`${url}?${urlQuery}`);
+	console.log(`${socrataUrl}?${socrataQuery + "&" + urlQuery}`);
 
 	if (zipcode.length > 0 && zipcode.length !== 5) {
 		return res.render("search.hbs", {errorMessage: `ERROR: Zip code should have 5 digits. You put in ${zipcode.length}`});
@@ -108,7 +115,7 @@ app.post('/search', (req, res) => {
 	// if zipcode contains letters, return errorMessage
 	// ADD CODE HERE
 	
-	request(`${url}?${urlQuery}`, (error, response, body) => {
+	request(`${socrataUrl}?${socrataQuery + "&" + urlQuery}`, (error, response, body) => {
 		if (JSON.parse(body).length === 0) {
 			return res.render("search.hbs", {
 				pageTitle: 'Search Results',
@@ -116,30 +123,39 @@ app.post('/search', (req, res) => {
 		}
 
 		if (!error && response.statusCode === 200) {
+			var searchResults = JSON.parse(body);
+
+			if (searchResults.length === 0) {
+				return res.render("search.hbs", {errorMessage: 'No results found.'});
+			} else {
+				res.render("search.hbs", {
+					pageTitle: 'Search Results',
+					body: searchResults});		
+			}	
 
 			// If user search includes restaurant name
-			if (businessName.length > 0) {
-				var searchResults = JSON.parse(body).filter((business) => {
-					if (business.dba) {
-						return business.dba.toLowerCase().includes(businessName);
-					}
-				});
-				console.log(`${searchResults.length} search results`);
+			// if (businessName.length > 0) {
+			// 	var searchResults = JSON.parse(body).filter((business) => {
+			// 		if (business.dba) {
+			// 			return business.dba.toLowerCase().includes(businessName);
+			// 		}
+			// 	});
+			// 	console.log(`${searchResults.length} search results`);
 
-				if (searchResults.length === 0) {
-					return res.render("search.hbs", {errorMessage: 'No results found.'});
-				} else {
-					res.render("search.hbs", {
-						pageTitle: 'Search Results',
-						body: searchResults});		
-				}				
-			} else {
-				console.log(`${url}?${urlQuery}`);
-				res.render("search.hbs", {
-					body: JSON.parse(body),
-					pageTitle: 'Search Results'
-				});
-			}
+			// 	if (searchResults.length === 0) {
+			// 		return res.render("search.hbs", {errorMessage: 'No results found.'});
+			// 	} else {
+			// 		res.render("search.hbs", {
+			// 			pageTitle: 'Search Results',
+			// 			body: searchResults});		
+			// 	}				
+			// } else {
+			// 	console.log(`${url}?${urlQuery}`);
+			// 	res.render("search.hbs", {
+			// 		body: JSON.parse(body),
+			// 		pageTitle: 'Search Results'
+			// 	});
+			// }
 		} else {
 			return error;
 		}		
@@ -153,6 +169,7 @@ app.get('/dba/:id', (req, res) => {
 
 	request(`${socrataUrl}?$where=DBA%20like%20%27%25${businessName}%25%27&$$app_token=${token}&boro=QUEENS&zipcode=11358`, (error, response, body) => {
 		var searchResults = JSON.parse(body);
+		console.log(`${socrataUrl}?$where=DBA%20like%20%27%25${businessName}%25%27&$$app_token=${token}&boro=QUEENS&zipcode=11358`);
 		console.log(`Your search returned ${searchResults.length} results`);
 		res.send(searchResults);
 	});

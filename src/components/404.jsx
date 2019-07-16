eff6b7069ca043cd90578ee9556930a63bb87b95
Gary Pang <gary.pang@gmail.com>
2019-07-16 15:44:31 -0400

    Modify margin top of parallax content
    Add margin to move content closer to parallax image's vertical center.
    
    Also, remove class "hide-on-small-only" from Parallax component, so
    Parallax appears on small screens.

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -3,11 +3,9 @@ import { Parallax } from "react-parallax";
 
 const NotFound = () => {
     return (
-        <div className="hide-on-small-only">
+        <div>
           <Parallax bgImage={"https://res.cloudinary.com/hg7jltnn9/image/upload/f_auto,q_auto/v1562031458/public/assets/img/kevin-curtis-3308-unsplash-1024x676_bcwbjg.jpg"} strength={500} style={{ 'height': "500px" }}>
-            {/* <div style={{ 'height': "150px" }} /> */}
-
-            <div className="container" style={{'backgroundColor': 'rgba(200,200,200,0.5)', 'padding': '30px'}}>
+            <div className="container" style={{'backgroundColor': 'rgba(200,200,200,0.5)', 'padding': '30px', 'marginTop': '100px'}}>
                 <h3 className="center" style={{'fontWeight': 'bold'}}>Page Not Found</h3>
                 <div className="row center">
                     <h5>It looks like you were looking for a page that does not exist.</h5>    			
@@ -20,6 +18,5 @@ const NotFound = () => {
         </div>
     )
 }
-
 
 export default NotFound;
